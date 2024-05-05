50e96751338182da4c2749deeb881eaf0f7de755
kewon96 <yang0184@naver.com>
2024-05-05 22:04:17 +0900

    Initial commit

---
+++ src/test/java/core/princple/spring_core_principle/SpringCorePrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package core.princple.spring_core_principle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorePrincipleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
