fffc704eb928d7268bae011dddb5800a0fe65271
kewon96 <yang0184@naver.com>
2024-05-06 02:18:04 +0900

    new: Member Package

---
+++ src/main/java/core/princple/spring_core_principle/domain/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package core.princple.spring_core_principle.domain.member.repository;
+
+import core.princple.spring_core_principle.domain.core.domain.DomainRepository;
+import core.princple.spring_core_principle.domain.member.model.Member;
+
+public sealed interface MemberRepository extends DomainRepository<Member> permits MemberMemoryRepository, MemberDatabaseRepository {
+}
